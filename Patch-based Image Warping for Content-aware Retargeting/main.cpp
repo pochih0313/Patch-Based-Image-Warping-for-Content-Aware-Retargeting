faf4af4e5aa5be69d30259d39040382f0a2cee28
t857057 <t857057@icloud.com>
2021-01-30 20:31:56 +0800

    Initial Commit

---
+++ Patch-based Image Warping for Content-aware Retargeting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Patch-based Image Warping for Content-aware Retargeting
+//
+//  Created by 陳柏志 on 2021/1/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
